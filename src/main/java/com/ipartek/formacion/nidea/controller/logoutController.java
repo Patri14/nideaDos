11c4e3962ca7413187ff3ea770a532fcba3b44d5
Curso <Curso@Formacion-TOSH3.ipartekaula.com>
2018-04-03 14:18:14 +0200

    martes 03/04/2018

---
+++ src/main/java/com/ipartek/formacion/nidea/controller/logoutController.java
@@ -0,0 +1,46 @@
+package com.ipartek.formacion.nidea.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class logoutController
+ */
+@WebServlet("/logout")
+public class logoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// borra la sesión
+		// guardar usuario en sesión
+		HttpSession session = request.getSession();
+		session.setAttribute("usuario", null);
+		session.invalidate();
+
+		// para que nos varie del logout a login en la url
+		response.sendRedirect("login");
+		// request.getRequestDispatcher("login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
