d675d278d13b16e5bdbdd032ce6451bb8ff5de9f
Curso <Curso@Formacion-TOSH3.ipartekaula.com>
2018-04-12 10:32:17 +0200

    act

--- src/main/java/com/ipartek/formacion/nidea/util/Utilidades.java
+++ src/main/java/com/ipartek/formacion/nidea/util/Utilidades.java
@@ -1,5 +1,10 @@
 package com.ipartek.formacion.nidea.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ipartek.formacion.nidea.ArrayListEjemplos.Ordenable;
+
 public class Utilidades {
 
 	/**
@@ -26,4 +31,49 @@ public class Utilidades {
 		return cadena;
 
 	}
+
+	/**
+	 * Ordenar una colecion con el algoritmo bubble sort, ordena de menor a mayor
+	 * basandose en el metodo getValor de la interfaz Ordenable
+	 * 
+	 * @see com.ipartek.formacion.nidea.ejemplos.Ordenable
+	 * @param coleccion
+	 *            List<Ordenable> coleccion con los elementos a ordenadar
+	 * @return en caso de null retornamos una lista vacia
+	 */
+	public static List<Ordenable> bubbleSort(List<Ordenable> coleccion) {
+		List<Ordenable> resul = new ArrayList<Ordenable>();
+		if (coleccion != null) {
+			Ordenable temp;
+			if (coleccion.size() > 1) // check if the number of orders is larger than 1
+			{
+				for (int x = 0; x < coleccion.size(); x++) // bubble sort outer loop
+				{
+					for (int i = 0; i < coleccion.size() - i; i++) {
+						if (coleccion.get(i).getValor() > (coleccion.get(i + 1).getValor())) {
+							temp = coleccion.get(i);
+							coleccion.set(i, coleccion.get(i + 1));
+							coleccion.set(i + 1, temp);
+						}
+					}
+				}
+			}
+
+			resul = coleccion;
+		}
+		return resul;
+	}
+
+	/**
+	 * if (list.size()>1) // check if the number of orders is larger than 1 { for
+	 * (int x=0; x<list.size(); x++) // bubble sort outer loop { for (int i=0; i <
+	 * list.size()-i; i++) { if (list.get(i).compareTo(list.get(i+1)) > 0) { temp =
+	 * list.get(i); list.set(i,list.get(i+1) ); list.set(i+1, temp); } } } }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
 }
